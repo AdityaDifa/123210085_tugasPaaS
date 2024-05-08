9cf619b4a5e162912bf5ddf70f496fe787aed97e
Aditya Difa Amanullah <adityadifa123@gmail.com>
2024-05-08 21:58:30 +0700

    first push

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const nilai = {
+    matematika : '96',
+    ipa : '90',
+    informatika : '100'
+}
+
+app.get('/',(req,res)=> {
+    res.json(profile);
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log("server sedang berjalan di port : ${PORT}");
+});
